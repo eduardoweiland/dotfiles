20ea5d99772f3eac9baebec4798d1ffc589fd2b6
Eduardo Weiland <eduardo@ew.dev.br>
2020-02-05 22:10:07 -0300

    Add basic configuration for tint2 panel

---
+++ .config/tint2/volume.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/sh
+# based on https://petermolnar.net/hacking-tint2-panel-weather-cpu-temperature-and-volume-executors/
+
+if [ "$1" == "up" ]; then
+    amixer -D pulse sset Master 5%+ &> /dev/null
+elif [ "$1" == "down" ]; then
+    amixer -D pulse sset Master 5%- &> /dev/null
+elif [ "$1" == "mute" ]; then
+    amixer -D pulse sset Master 1+ toggle
+fi
+
+mixer_output=$(amixer sget -D pulse Master)
+
+volume=$(grep -Pom1 '\d+(?=%)' <<< $mixer_output)
+status=$(grep -Pom1 'off|on'   <<< $mixer_output)
+
+icon_dir=/usr/share/icons/Flat-Remix-Blue-Dark/status/symbolic
+
+if [ "$status" == "off" ]; then
+    icon=muted
+elif [ $volume -gt 0 ] && [ $volume -lt 34 ]; then
+    icon=low
+elif [ $volume -gt 33 ] && [ $volume -lt 67 ]; then
+    icon=medium
+else
+    icon=high
+fi
+
+icon_path=$icon_dir/audio-volume-$icon-symbolic.svg
+
+echo $icon_path
+
+# @TODO: discover a way to replace existing notification
+#if [ ! -z "$1" ]; then
+#    notify-send -t 2000 -i $icon_path -c device "Volume" "Volume is set to $volume%"
+#fi
