c0dbb922506b5a59c6374b32f193b0b8f2184745
Eduardo Weiland <eduardo.weiland@quantosobra.com.br>
2023-01-16 10:47:48 -0300

    Disable search highlight after substitute

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,15 @@
 require('options')
 require('plugins')
 require('mappings')
+
+-- Disable search highlight after substitute
+local auto_searchhl = vim.api.nvim_create_augroup('auto_searchhl', { clear = true })
+vim.api.nvim_create_autocmd('CmdlineLeave', {
+  pattern = ':',
+  callback = function()
+    if (vim.fn.getcmdline():find('s/')) then
+      vim.defer_fn(function() vim.cmd([[nohlsearch]]) end, 0)
+    end
+  end,
+  group = auto_searchhl,
+})
