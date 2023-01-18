bcfcc19eb5d12267446e2d0dface5ff141270eb0
Eduardo Weiland <eduardo.weiland@quantosobra.com.br>
2023-01-18 13:27:20 -0300

    Add mappings for switching and closing buffers

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -34,3 +34,8 @@ map('n', '<leader>dd', '"+dd', { noremap = true })
 -- Toggle NvimTree
 map('n', '<leader>b', ':NvimTreeToggle<CR>', { silent = true })
 map('v', '<leader>b', ':NvimTreeToggle<CR>', { silent = true })
+
+-- Switching/closing buffers
+map('n', 'bd', ':Bdelete<CR>', { silent = true, noremap = true })
+map('n', 'bn', ':bnext<CR>', { silent = true, noremap = true })
+map('n', 'bp', ':bprevious<CR>', { silent = true, noremap = true })
