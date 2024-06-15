dd921e6d5945200017820237bf825f1e2357d0d5
Eduardo Weiland <eduardo@ew.dev.br>
2024-06-15 15:49:07 -0300

    change neovim theme variant and sync changes to tmux and alacritty

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,7 +2,10 @@ return {
   {
     'navarasu/onedark.nvim',
     opts = {
-      style = 'warmer',
+      style = 'darker',
+      colors = {
+        bg0 = '#1e2127',
+      },
     },
     config = function(_, opts)
       require('onedark').setup(opts)
